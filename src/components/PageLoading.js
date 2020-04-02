d44f528e3682cbc476154d04b8a8d7b12fde4100
Facu <facuaaraujo@gmail.com>
2020-04-02 16:05:48 -0300

    Cree un Loader y un componente contenedor para cuando funciona para una pagina entera

---
+++ src/components/PageLoading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles/PageLoading.scss';
+
+import Loader from './Loader';
+
+const PageLoading = () => (
+  <div className="PageLoading">
+    <Loader />
+  </div>
+);
+
+export default PageLoading;
