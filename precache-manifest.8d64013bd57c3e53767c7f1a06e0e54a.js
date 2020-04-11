9bc113c4d3f34d02eb6929c610677e239d3745f2
Facundo Araujo <facuaaraujo@gmail.com>
2020-04-11 03:01:36 -0300

    Updates

---
+++ precache-manifest.8d64013bd57c3e53767c7f1a06e0e54a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5acbc53c6ac1af1d8a87dc3f3576a2ea",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "47243d6c14e80cf1705b",
+    "url": "/twitter-clone/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "c677fb3bf399c40df05a",
+    "url": "/twitter-clone/static/css/main.817ff423.chunk.css"
+  },
+  {
+    "revision": "47243d6c14e80cf1705b",
+    "url": "/twitter-clone/static/js/2.7c17fbb4.chunk.js"
+  },
+  {
+    "revision": "ad61e5a3703d8af976e070e9896b739a",
+    "url": "/twitter-clone/static/js/2.7c17fbb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c677fb3bf399c40df05a",
+    "url": "/twitter-clone/static/js/main.5a3f726d.chunk.js"
+  },
+  {
+    "revision": "06ff6d213d6ae1091a14",
+    "url": "/twitter-clone/static/js/runtime-main.ab6ac535.js"
+  },
+  {
+    "revision": "313656d46d6613b2d25f50a785361e1c",
+    "url": "/twitter-clone/static/media/corazon-like.313656d4.png"
+  },
+  {
+    "revision": "3ec228d3193537d0fb027a5f0a0a0a79",
+    "url": "/twitter-clone/static/media/google-logo.3ec228d3.svg"
+  },
+  {
+    "revision": "672fae9763976b1a3f64405c31629cd5",
+    "url": "/twitter-clone/static/media/twitter-logo.672fae97.svg"
+  }
+]);
